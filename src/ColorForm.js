6196491b1c0294aba68e384e0412eb05ac46d8ba
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-03-14 12:31:50 -0400

    First Commit

---
+++ src/ColorForm.js
@@ -0,0 +1,25 @@
+
+import React, { useState } from "react";
+
+function ColorForm (props) {
+    let [input, setInput] = useState('')
+
+    const handleSubmit = (e) => {
+        //prevent default form behavior
+        e.preventDefault ()
+        //addcolor available w/ props.addColor
+        props.addColor(input)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type='text'
+                onChange={ (e) => setInput(e.target.value)} />
+                <button type="submit">Submit!</button>
+            </form>
+        </div>
+    )
+}
+
+export default ColorForm;
